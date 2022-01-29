913bfc19ea00b5a466306f44dd7480371ab8b060
vigneshkanna <vkrishitaran@gmail.com>
2022-01-29 17:03:33 +0530

    Reactjs boiler codes cleaned

---
+++ netflix-react/src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">Netflix clone 🎥</div>;
+}
+
+export default App;
---
+++ netflix-react/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+// ReactJS starting code which gets root element from index.html and renders the components above it!!!
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
