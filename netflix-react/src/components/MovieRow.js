850a69349e188dc87a7cdb82aa35ccf58ebd4316
vigneshkanna <vkrishitaran@gmail.com>
2022-01-30 21:01:12 +0530

    movie poster rows added

---
+++ netflix-react/src/components/MovieRow.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "../middleware/Axios";
+
+// Importing this file CSS
+import "./movierow.css";
+
+const MovieRow = ({ title, fetchUrl, isLarge }) => {
+  const [movies, setmovies] = useState([]);
+
+  // Code snippet to run for each page load or fetchUrl changes
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(fetchUrl);
+      console.log(response.data.results);
+      setmovies(response.data.results);
+      return response;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="movie_row">
+      <h3 className="row_heading">{title}</h3>
+      <div className="row_posters">
+        {movies.map((movie) => {
+          return (
+            <img
+              src={`https://image.tmdb.org/t/p/original${isLarge ? movie.poster_path : movie.backdrop_path}`}
+              alt="img"
+              className={`row_poster ${isLarge && "row_posterLarge"}`}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MovieRow;
